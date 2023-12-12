2e34f7347ef95910c9bb1ee6af777cda592e8281
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-12-12 12:32:06 +0100

    updated index.js

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var userGenerator_1 = require("./userGenerator");
+//From there we get a random list of 50 users generated randomly:
 var usersArray = (0, userGenerator_1.default)();
 function matching(userA, userArray) {
-    var prefs = userA.userPrefs;
+    var prefs = userA.userPrefs; //Stores the user's prefs in a variable
+    //First we check if the generated users match with Sample User's prefs, otherwise they won't show up in the matching process
     userArray.map(function (user) {
         if (prefs.age.includes(user.age)) {
+            //Checking if the user's languages match with the Sample User's prefs
             user.languages.map(function (l) {
                 return prefs.languages.includes(l) ? user.affinityScore++ : "";
-            });
+            } // Every matching statement adds 1 point to the user's affinity score
+            );
+            //Same with hobbies
             user.hobbies.map(function (h) {
                 return userA.hobbies.includes(h) ? user.affinityScore++ : "";
             });
+            //Checks if user's gender matches with Sample User's prefs
             if (prefs.gender.includes(user.gender)) {
                 user.affinityScore++;
             }
+            //Checks if user's location matches with Sample User's location (no coordinates here, just a number representing the distance)
             if (prefs.location.includes(user.location)) {
                 user.affinityScore++;
             }
+            //If the user's affinity score is greater than 3, it's a top profile, it logs the user's info
             if (user.affinityScore > 3) {
                 console.log("\n".concat(user.name, " is a top profile for ").concat(userA.name, "\n scored: ").concat(user.affinityScore, " points! \nThe profile's \nlanguages: ").concat(user.languages, ", \nhobbies: ").concat(user.hobbies, ", \nlocation: ").concat(user.location, "\n"));
+                //If the user's affinity score is less than 3, it only logs the affinity score
             }
             else {
                 console.log("".concat(userA.name, " affinity score with ").concat(user.name, " is : ").concat(user.affinityScore));
@@ -27,8 +36,9 @@ function matching(userA, userArray) {
         }
     });
 }
-var charlesEdouard = {
-    name: "Charles-Édouard",
+//You can edit this user with values from hobbies.ts and languages.ts
+var sampleUser = {
+    name: "Sample User",
     age: 25,
     hobbies: [
         "taekwondo",
@@ -48,5 +58,5 @@ var charlesEdouard = {
         location: Array.from({ length: 100 - 0 + 1 }, function (_, i) { return i; }),
     },
 };
-matching(charlesEdouard, usersArray);
+matching(sampleUser, usersArray);
 exports.default = matching;
